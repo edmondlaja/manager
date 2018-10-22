05e7f06897766a529d318bc8f63d515d13b32b7f
Edmond Laja <>
2018-10-22 22:44:57 +0200

    commit for all

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+import Router from './Router';
+
+class App extends Component{
+
+    componentWillMount() {
+        const config = {
+            apiKey: "AIzaSyAf9PbXbqc81P2YEuhY8bhWKiucBit_NxM",
+            authDomain: "manager-b10db.firebaseapp.com",
+            databaseURL: "https://manager-b10db.firebaseio.com",
+            projectId: "manager-b10db",
+            storageBucket: "manager-b10db.appspot.com",
+            messagingSenderId: "754975068179"
+        };
+        firebase.initializeApp(config);
+    }
+
+    render() {
+        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+        return(
+            <Provider store={store}>
+                <Router />
+            </Provider>
+        );
+    }
+}
+
+export default App;
